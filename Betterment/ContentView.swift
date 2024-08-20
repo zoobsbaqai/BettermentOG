d3bb97287d36194228dc24f9ce5fc94b459a7ad1
zoobsbaqai <zohairbaqai@hotmail.com>
2024-08-20 03:17:09 -0400

    Day 5
    Almost completed home-screen, need to do finishing touches and add navigation to buttons, aswell as add an info button for tomorrow

--- Betterment/ContentView.swift
+++ Betterment/ContentView.swift
@@ -29,24 +29,24 @@ struct ContentView: View {
                             .position(CGPoint(x: 350.0, y: 30.0))
                     }
                     
-                    RoutPlans()
+                    RoutPlanScroll()
                         .position(x: 197, y: 600)
                     
                     Text("Routine Plans")
                         .font(.custom("Fredoka-SemiBold", size: 36))
                         .position(CGPoint(x: 130.0, y: 195.0))
-                 
+                    
                     
                     HStack {
                         Text("Better")
                             .foregroundStyle(.yellow)
                             .font(.custom("Fredoka-SemiBold", size: 56))
-                            .position(CGPoint(x: 95.0, y: 50.0))
+                            .position(CGPoint(x: 95.0, y: 45.0))
                         
                         Text("ment")
                             .foregroundStyle(.white)
                             .font(.custom("Fredoka-SemiBold", size: 56))
-                            .position(CGPoint(x: 38, y: 50))
+                            .position(CGPoint(x: 38, y: 45))
                     }
                     
                     VStack {
@@ -54,7 +54,7 @@ struct ContentView: View {
                             .foregroundStyle(.white)
                             .font(.custom("Fredoka-SemiBold", size: 35))
                             .position(CGPoint(x: 195, y: 100.0))
-                    
+                        
                     }
                 }
             }
@@ -87,21 +87,78 @@ struct SettingsView: View {
     }
 }
 
-struct RoutPlans: View {
+struct RoutPlanScroll: View {
     
     let imageNames = ["TemplateCard", "TemplateCard", "TemplateCard"]
+    let buttonNames = ["LetsGo", "LetsGo", "LetsGo"]
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
-                ForEach(imageNames, id: \.self) { imageName in
-                    Image(imageName)
+            ZStack{
+                VStack(spacing: 20) {
+                    ForEach(imageNames, id: \.self) { imageName in
+                        Image(imageName)
+                        
+                    }
+                    
+                    Spacer()
+                        .frame(height: 190)
                 }
-                Spacer()
-                    .frame(height: 200)
+                VStack(spacing: 20) {
+                    ForEach(buttonNames, id: \.self) {buttonName in
+                        Image(buttonName)
+                            .position(CGPoint(x: 70.0, y: 265.0))
+                        Spacer()
+                            .frame(height: 230)
+                    }
+                    Spacer()
+                        .frame(height: 190)
+                }
+                Text("Project 50")
+                    .font(.custom("Fredoka-SemiBold", size: 28))
+                    .foregroundStyle(.black)
+                    .position(CGPoint(x: 90.0, y: 216))
+                Text("75 Hard")
+                    .font(.custom("Fredoka-SemiBold", size: 28))
+                    .foregroundStyle(.black)
+                    .position(CGPoint(x: 72.5, y: 545.0))
+                Text("Betterment30")
+                    .font(.custom("Fredoka-SemiBold", size: 28))
+                    .foregroundStyle(.black)
+                    .position(CGPoint(x: 110.0, y: 876))
+                Image("p50thumbnail")
+                    .position(CGPoint(x: 185.0, y: 0.0))
+                    .scaleEffect(0.825)
+                Image("75hardthumb")
+                    .position(CGPoint(x: 90.0, y: 375.0))
+                    .scaleEffect(0.75)
+                Image("bettermentlogo-transformed")
+                    .scaleEffect(0.2)
+                    .position(CGPoint(x: 104.0, y: 765.0))
             }
             .padding()
         }
+        .fadeOutTop()
+        
+    }
+}
+
+extension View {
+    func fadeOutTop(fadeLength:CGFloat=25) -> some View {
+        return mask(
+            VStack(spacing: 0) {
+                
+                // Top gradient
+                LinearGradient(gradient:
+                                Gradient(
+                                    colors: [Color.black.opacity(0), Color.black]),
+                               startPoint: .top, endPoint: .bottom
+                )
+                .frame(height: fadeLength)
+                
+                Rectangle().fill(Color.black)
+            }
+        )
     }
 }
 
@@ -110,4 +167,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-    
+
